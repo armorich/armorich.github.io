a9aac0012dfd300436b39c87927c0a5848634506
Artyom <56970729+armorich@users.noreply.github.com>
2023-09-22 14:07:12 +0300

    Create script.js

---
+++ food/js/script.js
@@ -0,0 +1,19 @@
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms'; 
+import slider from './modules/slider';
+import { openModal } from './modules/modal';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+    tabs(); 
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer(); 
+    cards(); 
+    calc(); 
+    forms(modalTimerId); 
+    slider(); 
+});
